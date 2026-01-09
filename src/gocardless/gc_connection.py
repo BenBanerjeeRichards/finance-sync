6a9efefde657187c1a3bee1108e5f0f32a4d5cd5
Ben Banerjee-Richards <5972559+BenBanerjeeRichards@users.noreply.github.com>
2026-01-09 22:23:43 +0000

    remove old code

--- src/gocardless/gc_connection.py
+++ src/gocardless/gc_connection.py
@@ -2,8 +2,6 @@ from gocardless.gocardless import GoCardlessClient
 from model import GcStore, Config
 from storage import Store, GC_STORE_FILE
 import logging
-from http.server import BaseHTTPRequestHandler, HTTPServer
-from urllib.parse import urlparse, parse_qs
 
 class MissingTransactionId(Exception):
     pass
@@ -41,71 +39,3 @@ class GcConnection:
         logging.info("Found %s existing requisitions to delete", len(prev_reqs))
         for prev_req in prev_reqs:
             self.client.delete_requisition(prev_req.id)
-
-    def serve(self):
-        server = ReqServer(('0.0.0.0', 8080), HttpHandler, self)
-        logging.info("GC server running on port 8080")
-        server.serve_forever()
-
-
-
-class ReqServer(HTTPServer):
-
-    def __init__(self, server_address, handler, sync: GcConnection):
-        super().__init__(server_address, handler)
-        self.sync = sync
-
-class HttpHandler(BaseHTTPRequestHandler):
-
-    def do_GET(self):
-        parsed_url = urlparse(self.path)
-        path = parsed_url.path
-        params = parse_qs(parsed_url.query)
-        if path.endswith("/start-requisition"):
-            link = self.server.sync.start_requisition()
-            self.send_response(302)
-            self.send_header("Location", link)
-            self.end_headers()
-        elif path.endswith("/complete-requisition"):
-            ref = params.get("ref", [None])[0]
-            status_html = "OK"
-
-            if params.get("error"):
-                logging.warning("Failed requisition %s - %s", ref,  params)
-                status_html = """
-                    Failed to compelete requisition: {} -  {}
-                """.format(params.get("error", [""])[0], params.get("details", [""])[0])
-
-            if ref is None:
-                logging.warning("No ref provided to complete-requisition")
-                status_html = "Error: No requisition specified"
-            elif not params.get("error"):
-                self.server.sync.complete_requisition(ref)
-            self.send_response(200)
-            self.send_header("Content-Type", "text/html")
-            self.end_headers()
-            self.wfile.write("""
-                <html>
-                    <head>
-                        <title>GoCardless Complete Requisition</title>
-                    </head>
-                    <body>
-                        <h1> GoCardless Complete Authentication </h1>
-                        {}
-                    </body>
-                </html>
-                """.format(status_html).encode())
-        else:
-            self.send_response(404)
-            self.send_header("Content-Type", "text/html")
-            self.end_headers()
-            self.wfile.write("""
-                <html>
-                    <head>
-                        <title>Not Found</title>
-                    </head>
-                    <body>
-                        <h1>Not Found</h1>
-                    </body>
-                </html>
-                """.encode())
