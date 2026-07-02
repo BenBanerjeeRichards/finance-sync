9c2cd097799ec1509fddb8284bcba9f4a1e01980
Ben Banerjee-Richards <5972559+BenBanerjeeRichards@users.noreply.github.com>
2026-07-02 21:23:25 +0100

    fix unneeded deletions triggering too many messages

--- src/beancount_sync/accrual.py
+++ src/beancount_sync/accrual.py
@@ -30,13 +30,14 @@ class BeancountAccruals:
     def process_accrual(self, rule: AccrualConfig):
         logging.info("Calculating accruals for rule %s", rule.metadata_key)
         settlements = self.beancount.find_all_by_metadata_by_date_desc(rule.metadata_key, VALUE_SETTLEMENT)
-        provisional_liabilities = self.beancount.find_all_by_metadata_by_date_desc(rule.metadata_key, VALUE_PROVISIONAL_LIABILITY)
+        provisional_liabilities = self.beancount.find_all_by_metadata_by_date_desc(rule.metadata_key,
+                                                                                   VALUE_PROVISIONAL_LIABILITY)
         if not settlements:
             # If we have not yet had any actual transactions, we have nothing to base the liabilities on
             logging.warning("No settlements found for rule %s, can't compute any liabilities", rule.metadata_key)
             return
 
-        new_transactions: list[BeancountTransaction] = []
+        settlement_transactions: list[BeancountTransaction] = []
 
         for settlement in settlements:
             settlement_key = settlement.meta.get("external_id")
@@ -59,28 +60,21 @@ class BeancountAccruals:
                                               rule.metadata_key: VALUE_LIABILITY
                                           }, source="accrual", amount=abs(liability_amounts[i]),
                                           metadata={})
-                new_transactions.append(tx)
-
-        # Delete any provisional liabilities to keep this idempotent
-        delete_tx_ids = [pl.meta["external_id"] for pl in provisional_liabilities]
-        if delete_tx_ids:
-            logging.info("%s: deleting transactions: %s", rule.name, delete_tx_ids)
+                settlement_transactions.append(tx)
 
         with self.beancount.transaction() as beancount_tx:
-            for tx in new_transactions:
+            for tx in settlement_transactions:
                 beancount_tx.create_or_update_transaction(self.config.accrualBeanFileName, tx)
-            for tx_id in delete_tx_ids:
-                beancount_tx.delete_transaction(self.config.accrualBeanFileName, tx_id)
 
         settlements = self.beancount.find_all_by_metadata_by_date_desc(rule.metadata_key, VALUE_SETTLEMENT)
         if not settlements:
             logging.warning("(provisional) no settlements found for rule %s", rule.name)
             return
         most_recent_settlement = settlements[0]
-        new_transactions = []
+        provisional_transactions = []
 
         diff = relativedelta(datetime.date.today(), most_recent_settlement.date)
-        months_since_last_settlement = (diff.years * 12) + diff.months + 1       # 1 to include current month
+        months_since_last_settlement = (diff.years * 12) + diff.months + 1  # 1 to include current month
 
         if months_since_last_settlement <= 0:
             logging.info("skipping provisional liabilities for %s due to no months since last settlement %s", rule.name,
@@ -90,13 +84,14 @@ class BeancountAccruals:
         # we include the last settlement month in case settlement occurs often doesn't include that month
         # this would lead to a gap
         provisional_liability_months = [(most_recent_settlement.date + relativedelta(months=n)).replace(day=1) for n in
-                            range(months_since_last_settlement)]
+                                        range(months_since_last_settlement)]
 
         # Take 10% more than the highest from recent transactions to be slightly pessimistic
         max_recent = max([abs(s.postings[0].units.number) for s in settlements[:4]])
         estimated_amount = Decimal(str(max_recent)) * Decimal('1.10')
         estimated_liability = split_money_decimal(estimated_amount, rule.settlement_months)[0]
-        logging.info("liability %s: computing provisional liabilities for month %s (amount %s)", rule.name, provisional_liability_months, estimated_liability)
+        logging.info("liability %s: computing provisional liabilities for month %s (amount %s)", rule.name,
+                     provisional_liability_months, estimated_liability)
         for i, liability_date in enumerate(provisional_liability_months):
             liability_key = f"provisional-{most_recent_settlement.meta["external_id"]}-{liability_date.isoformat()}"
             tx = BeancountTransaction(external_id=liability_key, tx_date=liability_date,
@@ -107,11 +102,21 @@ class BeancountAccruals:
                                       ledger_metadata={
                                           rule.metadata_key: VALUE_PROVISIONAL_LIABILITY
                                       }, source="accrual", amount=abs(estimated_liability))
-            new_transactions.append(tx)
+            provisional_transactions.append(tx)
+
+        # Find any provisional liabilities that exist in journal but not created here
+        # These should be deleted to ensure this process is idempotent
+        new_transaction_ids = [tx.external_id for tx in provisional_transactions]
+        existing_provisional_ids = [pl.meta["external_id"] for pl in provisional_liabilities]
+        delete_tx_ids = set(existing_provisional_ids) - set(new_transaction_ids)
+        if delete_tx_ids:
+            logging.warning("%s: deleting transactions: %s", rule.name, delete_tx_ids)
 
         with self.beancount.transaction() as beancount_tx:
-            for tx in new_transactions:
+            for tx in provisional_transactions:
                 beancount_tx.create_or_update_transaction(self.config.accrualBeanFileName, tx)
+            for tx_id in delete_tx_ids:
+                beancount_tx.delete_transaction(self.config.accrualBeanFileName, tx_id)
 
 
 def split_money_decimal(total_amount, n):
